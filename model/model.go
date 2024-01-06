9bb9e43e9d49e3118862460b47f34cfed60b9107
Ken Wu <kenwu1009us@gmail.com>
2024-01-05 17:30:17 -0800

    add data model

---
+++ model/model.go
@@ -0,0 +1,24 @@
+package model
+
+type Receipt struct {
+	Retailer string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Total string `json:"total"`
+	Items []Item `json:"items"`
+
+}
+
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price string `json:"price"`
+}
+
+type ReceiptResponse struct {
+	ID string `json:"id"`
+}
+
+type PointsResponse struct {
+	Points string `json:"points"`
+}
+
